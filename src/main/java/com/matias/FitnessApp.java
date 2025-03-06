9d43382602091e741668645fe5039583911f0e4b
matiloti <60507817+ZaderCode@users.noreply.github.com>
2025-03-06 22:46:47 +0100

    feat(setup): initial project setup
    - Added files to .gitignore
    - Added required dependencies to pom.xml
    - Created main class FintnessApp.java for SpringBoot execution

---
+++ src/main/java/com/matias/FitnessApp.java
@@ -0,0 +1,13 @@
+package com.matias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessApp.class, args);
+	}
+	
+}
